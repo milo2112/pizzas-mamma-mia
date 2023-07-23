66c2e6a163459d93c1b002de65ecfe574d236f45
milo2112 <mjrp.ros@gmail.com>
2023-07-23 15:25:09 -0400

    primer commit y scaffolding del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { ENDPOINT } from './constants/constants'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>Bienvenidos a la pizza mamma mia!</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/constants/constants.js
@@ -0,0 +1,1 @@
+export const ENDPOINT = '../assets/apidata/pizzas.json' 
